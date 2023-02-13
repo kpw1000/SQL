50b4da2a14d88727bbd6b5e72968c7129a1ff892
Kevin P Walters <40248145+kpw1000@users.noreply.github.com>
2023-02-13 17:41:00 -0500

    Changed field name from sort_order to employersizesequence

--- CREATE TABLE for employersizes.sql
+++ CREATE TABLE for employersizes.sql
@@ -6,15 +6,15 @@
 CREATE TABLE IF NOT EXISTS public.employersizes
 (
     employersize character varying COLLATE pg_catalog."default" NOT NULL,
-    sort_order integer,
+    employersizesequence integer,
     CONSTRAINT employersizes_pkey PRIMARY KEY (employersize)
 )
 TABLESPACE pg_default;
 
-INSERT INTO employersizes( employersize, sort_order ) VALUES ( '5000 to 19,999', 5 ) ;
-INSERT INTO employersizes( employersize, sort_order ) VALUES ( '500 to 999', 3 ) ;
-INSERT INTO employersizes( employersize, sort_order ) VALUES ( '20,000 or more', 6 ) ;
-INSERT INTO employersizes( employersize, sort_order ) VALUES ( 'Not Provided', 7 ) ;
-INSERT INTO employersizes( employersize, sort_order ) VALUES ( '1000 to 4999', 4 ) ;
-INSERT INTO employersizes( employersize, sort_order ) VALUES ( 'Less than 250', 1 ) ;
-INSERT INTO employersizes( employersize, sort_order ) VALUES ( '250 to 499', 2 ) ;
+INSERT INTO employersizes( employersize, employersizesequence ) VALUES ( '5000 to 19,999', 5 ) ;
+INSERT INTO employersizes( employersize, employersizesequence ) VALUES ( '500 to 999', 3 ) ;
+INSERT INTO employersizes( employersize, employersizesequence ) VALUES ( '20,000 or more', 6 ) ;
+INSERT INTO employersizes( employersize, employersizesequence ) VALUES ( 'Not Provided', 7 ) ;
+INSERT INTO employersizes( employersize, employersizesequence ) VALUES ( '1000 to 4999', 4 ) ;
+INSERT INTO employersizes( employersize, employersizesequence ) VALUES ( 'Less than 250', 1 ) ;
+INSERT INTO employersizes( employersize, employersizesequence ) VALUES ( '250 to 499', 2 ) ;
