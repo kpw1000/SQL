7ccab4ce2ab085e0f224c7e97e35ebbd6823bca8
Kevin P Walters <40248145+kpw1000@users.noreply.github.com>
2023-03-05 10:23:31 -0500

    Update CREATE VIEW vw_gpg_sic_codes.sql

--- CREATE VIEW vw_gpg_sic_codes.sql
+++ CREATE VIEW vw_gpg_sic_codes.sql
@@ -4,19 +4,29 @@
 --* ************************************************************************************************
 CREATE VIEW vw_gpg_sic_codes AS
 	WITH RECURSIVE "gpg_siccode_recurs" AS
-	(
+		(
 		SELECT 
 			1 AS "counter"
 			, x.gpgkey
 			, x.employerid
-			--* In the Office for National Statistics, all SIC codes are five digits
+
+			--* The recursive query will extract SIC codes from this field.
+			, x.siccode_array
+
+			--* The siccodes field is delimited by a command and a line-feed. The inner query
+			--* below creates an array using the line-feed. Here we remove the comma and pad 
+			--* each sic code to five characters. In the Office for National Statistics, all 
+			--* SIC codes are five digits.
 			, RIGHT( '00000' || REPLACE( x.siccode_array[1], ',', '' ), 5 ) AS "siccode"
+
+			--* In the recursion query, we will limit recursion to the number of elements
+			--* in the array. So we need a count of array elements.
 			, ARRAY_LENGTH( x.siccode_array, 1 ) AS "siccode_count"
+
 		FROM (
 				SELECT
 					gpgkey
 					, employerid
-					, siccodes
 					, STRING_TO_ARRAY( siccodes, CHR(10) ) AS "siccode_array"
 				FROM gender_pay_gap
 
@@ -24,12 +34,21 @@ CREATE VIEW vw_gpg_sic_codes AS
 				--* Filter for certain keys during testing. These keys
 				--* were chosen as they have 0, 1, 2, or 4 SIC Codes.
 				--* ****************************************************
+				-- WHERE
+				--	g.employerid IN ( 
+				--		'18159', '19070', '16958', '771', '773', 
+				--		'687', '14495', '19421', '18232', '77', 
+				--		'771', '650', '19795', '840', '880',
+				--		'904', '19574', '20576', '20579', '496'
+				--	)
+
 				-- WHERE gpgkey IN ( 13, 14, 26, 27, 35, 40, 76, 84, 101, 171 )
 				--* ****************************************************
 				--* 
 				--* ****************************************************
 
 			) AS x
+
 		WHERE 
 			ARRAY_LENGTH( x.siccode_array, 1 ) >= 1
 
@@ -37,28 +56,21 @@ CREATE VIEW vw_gpg_sic_codes AS
 			AND REPLACE( x.siccode_array[1], ',', '' ) NOT IN ( '0', '1' )
 
 		UNION ALL SELECT
-			z.counter + 1 AS "counter"
+			y.counter + 1 AS "counter"
 			, y.gpgkey
 			, y.employerid
-			--* In the Office for National Statistics, all SIC codes are five digits
-			, RIGHT( '00000' || REPLACE( y.siccode_array[z.counter + 1], ',', '' ), 5 ) AS "siccode"
-			, ARRAY_LENGTH( y.siccode_array, 1 ) AS "siccode_count"
-		FROM (
-				SELECT
-					gpgkey
-					, employerid
-					, siccodes
-					, STRING_TO_ARRAY( siccodes, CHR(10) ) AS "siccode_array"
-				FROM gender_pay_gap
-			) AS y
-			INNER JOIN gpg_siccode_recurs z ON y.gpgkey = z.gpgkey
+			, y.siccode_array
+			, RIGHT( '00000' || REPLACE( y.siccode_array[y.counter + 1], ',', '' ), 5 ) AS "siccode"
+			, y.siccode_count
+
+		FROM gpg_siccode_recurs y
 
 		WHERE 
-			ARRAY_LENGTH( y.siccode_array, 1 ) >= z.counter + 1
+			ARRAY_LENGTH( y.siccode_array, 1 ) >= y.counter + 1
 
 			--* SIC Codes of '0' and '1' are not legitimate SIC Codes.
-			AND REPLACE( y.siccode_array[z.counter + 1], ',', '' ) NOT IN ( '0', '1' )
-	)
+			AND REPLACE( y.siccode_array[y.counter + 1], ',', '' ) NOT IN ( '0', '1' )
+		)
 
 	SELECT 
 		gpgkey
